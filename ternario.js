d9f9531a81dfc370683ce683d539ef05b6a2f31f
amandaolenski <amanda.olenski.santos@escola.pr.gov.br>
2026-03-13 10:47:13 +0000

    template

---
+++ ternario.js
@@ -0,0 +1,10 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+// se idadeCliente for maior que idadeMinima então faça
+if (idadeCliente >= idadeMinima){
+    console.log("Cerveja");
+}else{ 
+    console.log("Suco");
+}
+         //
---
+++ type-boolean.js
@@ -0,0 +1,17 @@
+// boolean
+//true -> verdadeiro
+//false -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 15;
+
+console.log(primeiroNumero === segundoNumero);
+
+const  texto1 = "Alura!";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
+
+//var
+
+var altura 
