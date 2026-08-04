658b646c34d40424033aa801010d49e22589ca62
amandaolenski <amanda.olenski.santos@escola.pr.gov.br>
2026-08-04 15:06:40 +0000

    acesso ponto

---
+++ objetos/acesso_ponto.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome = "André",
+    idade: 32,
+    cpf: "1122233345"
+    email: "andre@dominio.com"
+};
+
+console.log(`Nome do cliente é $ {cliente.nome} e essa pessoa tem {cliente.idade} anos.`)
--- objetos/objeto.js
+++ objetos/objeto.js
@@ -3,3 +3,9 @@ const listaCPF = [1111111, 2222222, 3333333];
 const informacoesPessoa = ["nome", "Amanda", "idade", 32, "CPF", "11112222333"];
 
 console.log(informacoesPessoa[1]);
+
+const objetoPessoa = {
+    idade = 16,
+    nome = "Amanda"
+};
+
